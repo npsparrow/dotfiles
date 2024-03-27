1ec9c81faaea6356f576338129fb758c66636fc2
Nikhil Panickssery <nikhil.panickssery@gmail.com>
2024-03-26 20:20:02 -0400

    oops I forgot something

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -2,7 +2,8 @@ let
   nikhil = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFugzgJ5Lq5yJS+Lt+5QxxWiKp1ResKflGIDit0FPOov";
   users = [ nikhil ];
 
-  systems = [];
+  sparrow = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMOTFCglBlDqZ6MhR642dpE/6pbNZaTkRQpWOH+OJzP";
+  systems = [ sparrow ];
 in
 {
   "rpi_pass.age".publicKeys = users ++ systems;
